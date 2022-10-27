0ff6b687a8ffd9807d626f087476048b99405285
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-28 00:02:59 +0300

    feat: implement main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import DefaultLayout from "./components/DefaultLayout";
+
+import HomePage from "./pages/HomePage";
+import AddressesPage from "./pages/AddressesPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <DefaultLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/address" element={<AddressesPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </DefaultLayout>
+    </Router>
+  );
 }
 
 export default App;
