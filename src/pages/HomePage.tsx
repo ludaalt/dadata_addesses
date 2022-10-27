c6dab39d52f2f95c39eb99435ccb9b0887be5757
Людмила Алтухова <luda.tamb@mail.ru>
2022-10-27 23:55:19 +0300

    feat: implement main app page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,26 @@
+import styled from "styled-components";
+
 import data from "../data/data";
+import { textConversion } from "../utils/textConversion";
+import BlockTitle from "../components/BlockTitle";
+
+const NewsText = styled.div`
+  font-family: "Montserrat Medium";
+
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+`;
 
 const HomePage = () => {
-  return <div>{data}</div>;
+  return (
+    <>
+      <BlockTitle title="Новости" />
+      <NewsText
+        dangerouslySetInnerHTML={{ __html: textConversion(data) }}
+      ></NewsText>
+    </>
+  );
 };
 
 export default HomePage;
